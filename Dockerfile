c43d2d407d473804babd3ebcb6a5a912444ccad5
jleveau <julien.leveau@etu.u-bordeaux.fr>
2018-04-09 18:35:08 +0200

    adding support for ssl certificated

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,6 @@ COPY --from=builder builder/dist /app/dist
 COPY --from=builder builder/server /app/server
 ENV NODE_ENV=prod
 RUN cd /app/server && npm install
-CMD cd /app/server && node server
+CMD cd /app/server && npm run-script start-prod
 EXPOSE 3000
+EXPOSE 443
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const path = require('path')
-
 const express = require('express')
 const app = express()
 var proxy = require('http-proxy-middleware');
@@ -7,25 +6,40 @@ const morgan = require('morgan')
 const http = require('http')
 const https = require('https')
 const fs = require('fs')
+const config = require("config")
+const forceSSL = require('express-force-ssl');
 
 // SSL configuration
-var sslOptions = {
-    key: fs.readFileSync('sslcerts/key.pem'),
-    cert: fs.readFileSync('sslcerts/cert.pem'),
-    passphrase: "J'aime la soupe de tortue !"
+var options = {
+  key: fs.readFileSync(config.sslOptions.key, "utf8"),
+  cert: fs.readFileSync(config.sslOptions.cert, "utf8"),
   };
-  
+if (config.sslOptions.passphrase) {
+  options.passphrase = config.sslOptions.passphrase
+}
+if (config.sslOptions.ca) {
+  options.ca = fs.readFileSync(config.sslOptions.ca, "utf8")
+}
+  console.log(options.passphrase )
 process.env.TZ = 'Europe/Paris' 
+
+app.use(forceSSL)
 app.use(express.static(path.resolve(path.join(__dirname, "../dist"))))
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "html")
 
 //logger
 app.use(morgan('dev'))
 
 //redirecting event app
-app.use('/api/events', proxy({target: 'http://localhost:3001', changeOrigin: true}));
+app.use('/api/events', proxy({target: 'http://' + config.services.events, changeOrigin: true}));
 
 //Serving angular application
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')))
 
-https.createServer(sslOptions, app).listen(3000)
+const server = http.createServer(app);
+const serverSSL = https.createServer(options, app);
+
+
+server.listen(3000);
+serverSSL.listen(443);
