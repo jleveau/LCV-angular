c09076e100531c067733b1c182be557a52a4ebd4
jleveau <julien.leveau@etu.u-bordeaux.fr>
2018-05-08 11:59:56 +0200

    improving responsive design

--- src/app/authentication/elements/auth.ts
+++ src/app/authentication/elements/auth.ts
@@ -3,11 +3,15 @@ export class Auth {
     password: string;
     confirm: string;
 
-    constructor() {}
+    constructor() { }
 
     validate() {
         if (this.confirm !== this.password) {
             return "Les mots de passes ne correspondent pas."
+        }
+        console.log(this.username.match(/^[a-zA-Z0-9]+([_ -]?[a-zA-Z0-9])*$/))
+        if (!this.username.match(/^[a-zA-Z0-9]+([_ -]?[a-zA-Z0-9])*$/)) {
+            return "Le nom d'utilisateur est invalide"
         }
         return null
     }
